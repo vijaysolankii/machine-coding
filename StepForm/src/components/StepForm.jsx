8647e13e15b851c964e37e48056ab1de18422561
vijaysolanki <vijay_solanki@gecg28.ac.in>
2024-02-14 23:58:19 +0530

    added boilerplate code inside step form

--- StepForm/src/components/StepForm.jsx
+++ StepForm/src/components/StepForm.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
 const StepForm = ({ stepsConfig = [] }) => {
-  console.log(stepsConfig)
+  
+  if(!stepsConfig.length){
+    return <></>
+  }
+  
   return <div>
     <h1>Step Form React</h1>
     <>
       {
         stepsConfig.map((step,index)=> (
-          <div key={step.stepName} className="step">
+          <div key={step.stepName} className="stepWrap">
             <span>{index+1}</span>
             <h2>{step.stepName}</h2>
-            <p>{step.component}</p>
+            <div className="step">{step.component}</div>
           </div>
         ))
       }
